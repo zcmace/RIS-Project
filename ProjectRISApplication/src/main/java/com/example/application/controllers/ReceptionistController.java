d7812c00c519447cfbc7e42f547aef67b83ca070
zcmace <zcmace@gmail.com>
2021-07-27 06:22:11 -0400

    removed redundant import statement

--- ProjectRISApplication/src/main/java/com/example/application/controllers/ReceptionistController.java
+++ ProjectRISApplication/src/main/java/com/example/application/controllers/ReceptionistController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.mail.javamail.JavaMailSender;
+
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.MailSendException;
 
